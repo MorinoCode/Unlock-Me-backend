7844274da15a994477cf515e9800dae53a70e031
MorinoCode <mortezamastercode@gmail.com>
2025-12-19 18:35:34 +0100

    allow render in cors

--- server.js
+++ server.js
@@ -13,9 +13,23 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'https://unlock-me-frontend.vercel.app'
+];
+
 app.use(cors({
-  origin: "http://localhost:5173",  
-  credentials: true                 
+  origin: function (origin, callback) {
+    
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true 
 }));
 app.use(express.json());
 app.use(cookieParser());

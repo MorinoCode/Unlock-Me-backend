68edffe688a1a08a711ad9af2667ddf867455f6d
MorinoCode <mortezamastercode@gmail.com>
2025-12-21 16:30:18 +0100

    get user details route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { protect } from "../middleware/auth.js";
-import { getExploreMatches, getUserLocation } from "../controllers/explore/explore.js";
+import { getExploreMatches, getUserDetails, getUserLocation } from "../controllers/explore/explore.js";
 
 
 
 const router = express.Router();
 
 router.get("/location", protect, getUserLocation);
-
 router.get("/explore", protect, getExploreMatches);
+router.get("/details/:userId", protect, getUserDetails);
 
 
 export default router;

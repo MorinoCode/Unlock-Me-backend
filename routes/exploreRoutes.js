a70d3c12eb1efdedb495cf11b926963f27c8a70a
MorinoCode <mortezamastercode@gmail.com>
2025-12-27 19:35:45 +0100

    add categories to profile page

--- routes/exploreRoutes.js
+++ routes/exploreRoutes.js
@@ -2,7 +2,6 @@
 import express from "express";
 import {
   getExploreMatches,
-  getMatchesDashboard,
   getUserDetails,
 } from "../controllers/explore/explore.js";
 import { protect } from "../middleware/auth.js";
@@ -11,7 +10,6 @@ const router = express.Router();
 
 router.get("/matches", protect, getExploreMatches);
 
-router.get("/matches-dashboard", protect, getMatchesDashboard);
 
 router.get("/user/:userId", protect, getUserDetails);
 

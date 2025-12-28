79451af43fe974b42ff9200080d23e43adf22ec3
MorinoCode <mortezamastercode@gmail.com>
2025-12-28 21:03:41 +0100

    better response for matching

--- routes/matchesRoutes.js
+++ routes/matchesRoutes.js
@@ -1,16 +1,10 @@
-// matchesRoutes.js
-
 import express from "express";
 import { protect } from "../middleware/auth.js";
-import { getMatchesDashboard } from "../controllers/matches/matches.js";
+import { getMatchesDashboard, getMatchInsights } from "../controllers/matches/matches.js";
 
 const router = express.Router();
 
-
-
 router.get("/matches-dashboard", protect, getMatchesDashboard);
-
-
-
+router.get("/insights/:targetUserId", protect, getMatchInsights);
 
 export default router;
